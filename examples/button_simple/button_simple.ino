5d2b945735bfc368531001f5d72d6e59a2d65b0e
iotool <roha.mbox@gmail.com>
2017-08-04 00:28:58 +0200

    Add files via upload

--- examples/button_simple/button_simple.ino
+++ examples/button_simple/button_simple.ino
@@ -1,1 +1,23 @@
-//
+/*
+ *  https://github.com/iotool/ArduinoButtonLibrary/examples/button_simple/
+ *  
+ *  History: 
+ *  2017-08-03 v1.0 first release
+ */
+ 
+#include <RohaButton.h>
+
+RohaButton pushButton(4);
+
+void setup() {
+  pinMode(13,OUTPUT);
+  digitalWrite(13,LOW);
+}
+
+void loop() {
+  if (pushButton.isPushed()) {
+    digitalWrite(13,HIGH);
+  } else {
+    digitalWrite(13,LOW);  
+  }
+}
